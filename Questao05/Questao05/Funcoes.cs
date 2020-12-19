ca2fe0dfc693431d713c11d8c74b8e2f5fd8b65a
hugotavaresinfnet <hugo.filho@al.infnet.edu.br>
2020-12-19 17:26:02 -0300

    Revert "ajuste projeto questao05"
    This reverts commit 19962d2e94f435c975c520dad585b0b7e51d45ae.

---
+++ Questao05/Questao05/Funcoes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao05
+{
+    static class Funcoes
+    {
+        public static DataTable readCSV(string filePath)
+        {
+            var dt = new DataTable();
+            // Creating the columns
+            foreach (var headerLine in File.ReadLines(filePath).Take(1))
+            {
+                foreach (var headerItem in headerLine.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    dt.Columns.Add(headerItem.Trim());
+                }
+            }
+
+            // Adding the rows
+            foreach (var line in File.ReadLines(filePath).Skip(1))
+            {
+                dt.Rows.Add(line.Split(';'));
+            }
+
+
+            return dt;
+        }
+
+
+        public static string GetCaminhoAplicacao()
+        {
+            return Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory.ToString())+"\\";
+        }
+    }
+}
